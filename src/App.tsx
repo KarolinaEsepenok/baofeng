ffae83f16ef2884729c09f9d7db9073a1935786b
karolinasmile@mail.ru <karolinasmile@mail.ru>
2022-12-12 17:59:38 +0300

    catalogItemCont

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import s from './App.module.scss';
 import Headers from "./components/Header/Headers";
 import Greating from "./components/Greating/Greating";
+import CatalogItems from "./components/Catalog/CatalogItems";
+import bf888ЫЫ from "./components/img/bf888ЫЫ.jpg"
 
 function App() {
   return (
@@ -10,6 +12,10 @@ function App() {
         <div className={s.centerApp}>
            <Headers/>
             <Greating/>
+            <div>
+                <CatalogItems img={bf888ЫЫ} nameItem={'Модель'} price={'30,00'} descriptionItem={'gdhdhhdhdh'}/>
+            </div>
+
         </div>
 
       ЭТО СТОИТ МИЛЛИОНЫ ДОЛЛАРОВ ХАХАХАХАХАХАХАХАХАХХАХА
---
+++ src/components/Catalog/CatalogItems.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import CardHeader from "@mui/material/CardHeader";
+import Avatar from "@mui/material/Avatar";
+import {red} from "@mui/material/colors";
+import IconButton, {IconButtonProps} from "@mui/material/IconButton";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import CardActions from "@mui/material/CardActions";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Collapse from "@mui/material/Collapse";
+import bf888ЫЫ from "../img/bf888ЫЫ.jpg"
+import s from "../Catalog/CommonItem/Catalog.module.scss"
+import {styled} from "@mui/material/styles";
+
+interface ExpandMoreProps extends IconButtonProps {
+    expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+}));
+export type CatalogItemsPropsType={
+    img:string
+    nameItem:string
+    price:string
+    descriptionItem:string
+
+}
+
+const CatalogItems = (props:CatalogItemsPropsType) => {
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+    return (
+        <div className={s.catalog}>
+            <div>
+                <h3>Наушники</h3>
+            </div>
+            <div className={s.item}>
+                <CardMedia
+                    component="img"
+                    height="194"
+                    image={props.img}
+                    alt="Paella dish"
+                />
+                <CardContent>
+                    <Typography variant="body2" color="text.secondary">{props.price}
+                    </Typography>
+                </CardContent>
+                <CardActions className={s.itemName} disableSpacing>
+                    <div>{props.nameItem}</div>
+                    <ExpandMore
+                        expand={expanded}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                    >
+                        <ExpandMoreIcon />
+                    </ExpandMore>
+                </CardActions>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent >
+                        <Typography className={s.itemDescr} paragraph>{props.descriptionItem}</Typography>
+
+                    </CardContent>
+                </Collapse>
+            </div>
+        </div>
+    );
+};
+
+export default CatalogItems;
