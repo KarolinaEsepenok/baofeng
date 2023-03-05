c2aaf4faf7e22a92e19f47b800e395f260107575
karolinasmile@mail.ru <karolinasmile@mail.ru>
2023-03-05 16:24:51 +0300

    fix: add link chancel

--- src/components/Catalog/Headphones/Headphones.tsx
+++ src/components/Catalog/Headphones/Headphones.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import s from "../Catalog.module.scss";
 import CatalogItems from "../CommonItem/CatalogItems";
 import walkie from '../../../assets/img/walkie.jpg';
-import batUV5R from '../../../assets/img/batUV5R.jpg'
+
 import {Link} from "react-router-dom";
 
 
@@ -10,22 +10,19 @@ export const Headphones = () => {
     return (
         <div>
             <div><Link to='/'>Назад</Link></div>
-        <div className={s.catalogContainer}>
+            <div className={s.catalogContainer}>
 
-         <div  className={s.nameLink}><h4 id={'headphones'} >
-             Наушники
-         </h4>
-         </div>
-            <div className={s.catalogItemMain}>
-                <CatalogItems img={walkie} nameItem={'Модель'} price={'30,00'} descriptionItem={'hdskjhf;dkj'}/>
-                <CatalogItems img={batUV5R} nameItem={'Модель'} price={'45,00'} descriptionItem={'gdhdhhdhdh'}/>
-                <CatalogItems img={walkie} nameItem={'Модель'} price={'10,00'} descriptionItem={'gdhdhhdhdh'}/>
-                <CatalogItems img={walkie} nameItem={'Модель'} price={'20,00'} descriptionItem={'gdhdhhdhdh'}/>
-                <CatalogItems img={walkie} nameItem={'Модель'} price={'20,00'} descriptionItem={'gdhdhhdhdh'}/>
-                <CatalogItems img={walkie} nameItem={'Модель'} price={'20,00'} descriptionItem={'gdhdhhdhdh'}/>
-                <CatalogItems img={walkie} nameItem={'Модель'} price={'20,00'} descriptionItem={'gdhdhhdhdh'}/>
-                <CatalogItems img={walkie} nameItem={'Модель'} price={'20,00'} descriptionItem={'gdhdhhdhdh'}/>
+                <div className={s.nameLink}><h4 id={'headphones'}>Наушники</h4></div>
 
-            </div></div></div>
+                <div className={s.catalogItemMain}>
+                    <CatalogItems img={walkie} nameItem={'Модель'} price={'30,00'} descriptionItem={'gdhdhhdhdh'}/>
+                    <CatalogItems img={walkie} nameItem={'Модель'} price={'30,00'} descriptionItem={'gdhdhhdhdh'}/>
+                    <CatalogItems img={walkie} nameItem={'Модель'} price={'30,00'} descriptionItem={'gdhdhhdhdh'}/>
+                    <CatalogItems img={walkie} nameItem={'Модель'} price={'30,00'} descriptionItem={'gdhdhhdhdh'}/>
+                    <CatalogItems img={walkie} nameItem={'Модель'} price={'20,00'} descriptionItem={'gdhdhhdhdh'}/>
+
+                </div>
+            </div>
+        </div>
     );
 };
