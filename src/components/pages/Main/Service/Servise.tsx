192e133e8b1f09f3641b795aace7d899d4d1795b
karolinasmile@mail.ru <karolinasmile@mail.ru>
2023-03-14 20:14:27 +0300

    fix: bag img/good

--- src/components/pages/Main/Service/Servise.tsx
+++ src/components/pages/Main/Service/Servise.tsx
@@ -4,8 +4,8 @@ import s from '../Greating/Greating.module.scss';
 export const Servise = () => {
     return (
         <div className={s.greatingService}>
-            <div >НОВИНКА! Услуга по настройке радиостанции!</div>
-            <button className={s.btn}>Заявка</button>
+            <div >Услуга по настройке радиостанции!</div>
+            <button className={s.btn}>Заказать</button>
         </div>
     );
 };
