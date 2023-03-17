5bbe6b52c1afcc972790bbff8d15b9419e4145ce
karolinasmile@mail.ru <karolinasmile@mail.ru>
2023-03-17 19:48:23 +0300

    fix: bag img/good

--- src/components/pages/Main/Greating/Greating.tsx
+++ src/components/pages/Main/Greating/Greating.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import s from './Greating.module.scss'
 import bf888 from '../../../../assets/img/bf888.png'
 import {ParticlesContainer} from '../../../Particles';
-import {Servise} from '../Service/Servise';
+
 
 
 const Greating = () => {
     return (
         <div>
-            <Servise/>
-
         <div id={'main'} className={s.greatingPage}>
             <div className={s.particles}><ParticlesContainer /></div>
             <div className={s.greatingName}>
