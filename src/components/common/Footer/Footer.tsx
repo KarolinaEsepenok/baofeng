1482e22ee2a0e8c1e1c4216fc94f66e9739db08c
karolinasmile@mail.ru <karolinasmile@mail.ru>
2023-03-28 18:33:47 +0300

    add: footer

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import s from './Footer.module.scss'
+import logoBaofeng from '../../../assets/img/logoBaofeng.svg';
 
 export const Footer = () => {
     return (
         <div className={s.footerContainer}>
-
+<div className={s.footerLogo}><img src={logoBaofeng} alt={'logo'}/></div>
+            <div><a></a><a></a><a></a><a></a>
+            </div>
+            <div><a></a></div>
+            <div className={s.footerSocial}><a></a></div>
 
         </div>
     );
