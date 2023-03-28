b841e9acdc6908af39787870e358b332f415bdd6
karolinasmile@mail.ru <karolinasmile@mail.ru>
2023-03-28 16:23:08 +0300

    add: footer scss

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import s from './App.module.scss';
 import {RoutesComponents} from './routes/RoutesComponents';
 import Greating from '../src/components/pages/Main/Greating/Greating';
 import Headers from '../src/components/common/Header/Headers';
+import {Footer} from '../src/components/common/Footer/Footer';
 
 
 
@@ -16,6 +17,7 @@ function App() {
             <Headers/>
             <Greating/>
            <RoutesComponents/>
+            <Footer/>
         </div>
     </div>
   );
