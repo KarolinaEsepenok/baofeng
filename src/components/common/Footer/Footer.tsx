e67f1f119edb499e24aec8a2dcf98b6ea2d86d43
karolinasmile@mail.ru <karolinasmile@mail.ru>
2023-03-29 21:43:12 +0300

    fix: footer scss

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import s from './Footer.module.scss'
 import logoBaofeng from '../../../assets/img/logoBaofeng.svg';
+import {Link} from "react-router-dom";
 
 export const Footer = () => {
     return (
         <div className={s.footerContainer}>
 <div className={s.footerLogo}><img src={logoBaofeng} alt={'logo'}/></div>
-            <div><a></a><a></a><a></a><a></a>
+            <div className={s.footerLinks}>
+                <Link  to={'/headphones'} >Наушники</Link>
+                <Link  to={'/antennas'}>Антенны</Link>
+                <Link  to={'/batteries'} >Аккумуляторы</Link>
+
             </div>
-            <div><a></a></div>
+            <div><Link  to='aboutUs' >О нас</Link></div>
             <div className={s.footerSocial}><a></a></div>
 
         </div>
