1d9b5295d1c350fdb4b9ba93852930f8bf3e88a1
karolinasmile@mail.ru <karolinasmile@mail.ru>
2023-03-30 16:55:04 +0300

    fix: footer scss

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import s from './Footer.module.scss'
 import logoBaofeng from '../../../assets/img/logoBaofeng.svg';
 import {Link} from "react-router-dom";
+import telegram from '../../../assets/img/telegram.svg'
+import viber from '../../../assets/img/viber.svg'
 
 export const Footer = () => {
     return (
@@ -13,8 +15,8 @@ export const Footer = () => {
                 <Link  to={'/batteries'} >Аккумуляторы</Link>
 
             </div>
-            <div><Link  to='aboutUs' >О нас</Link></div>
-            <div className={s.footerSocial}><a></a></div>
+            <div className={s.footerLinks}><Link  to='aboutUs' >О нас</Link></div>
+            <div className={s.footerSocial}><a><img alt={'telegram'}src={telegram}/></a><a><img src={viber} alt={viber}/></a></div>
 
         </div>
     );
