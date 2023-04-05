42bf73e6728bd9524e3259df027e96e613f533ae
karolinasmile@mail.ru <karolinasmile@mail.ru>
2023-04-05 12:39:40 +0300

    add:tooltip

--- src/components/pages/Main/Questions/Questions.tsx
+++ src/components/pages/Main/Questions/Questions.tsx
@@ -65,8 +65,7 @@ export const Questions= ()=> {
                 </AccordionSummary>
                 <AccordionDetails>
                     <Typography>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                        malesuada lacus ex, sit amet blandit leo lobortis eget.
+                        Lorem ipsum d
                     </Typography>
                 </AccordionDetails>
             </Accordion>
