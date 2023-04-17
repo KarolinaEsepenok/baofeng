5a01900816a73ae1384ce84d4cda71105e372569
karolinasmile@mail.ru <karolinasmile@mail.ru>
2023-04-17 21:29:37 +0300

    add:catalogItems

--- src/components/Catalog/Antennas/Antennas.tsx
+++ src/components/Catalog/Antennas/Antennas.tsx
@@ -33,6 +33,9 @@ export const Antennas = () => {
                 <CatalogItems  img={walkie}nameItem={'Основные характеристики'}price={'20,00'} descriptionItem={'gdhdhhdhdh'}/>
                 <CatalogItems  img={walkie}nameItem={'Основные характеристики'} price={'30,00'} descriptionItem={'gdhdhhdhdh'}/>
                 <CatalogItems  img={walkie}nameItem={'Основные характеристики'}price={'20,00'} descriptionItem={'gdhdhhdhdh'}/>
+                <CatalogItems  img={walkie}nameItem={'Основные характеристики'}price={'20,00'} descriptionItem={'gdhdhhdhdh'}/>
+                <CatalogItems  img={walkie}nameItem={'Основные характеристики'} price={'30,00'} descriptionItem={'gdhdhhdhdh'}/>
+                <CatalogItems  img={walkie}nameItem={'Основные характеристики'}price={'20,00'} descriptionItem={'gdhdhhdhdh'}/>
                 <ReactTooltip
                     anchorId="appTitle"
                     place="bottom"
