ac8b26667aa7ceec1b0695cbd700629009dac847
karolinasmile@mail.ru <karolinasmile@mail.ru>
2023-04-24 20:42:04 +0300

    add:catalogItems

--- src/components/Catalog/Battery/Battery.tsx
+++ src/components/Catalog/Battery/Battery.tsx
@@ -24,8 +24,7 @@ export const Battery = () => {
                     <CatalogItems img={walkie} nameItem={'Основные характеристики'} price={'28,00'} descriptionItem={'gdhdhhdhdh'}/>
                     <CatalogItems img={walkie} nameItem={'Основные характеристики'} price={'34,00'} descriptionItem={'gdhdhhdhdh'}/>
                     <CatalogItems img={walkie} nameItem={'Основные характеристики'} price={'34,00'} descriptionItem={'gdhdhhdhdh'}/>
-                    <CatalogItems img={walkie} nameItem={'Основные характеристики'} price={'20,00'} descriptionItem={'gdhdhhdhdh'}/>
-                    <CatalogItems img={walkie} nameItem={'Основные характеристики'} price={'34,00'} descriptionItem={'gdhdhhdhdh'}/>
+
 
                 </div></div>
         <Footer/></div>
