917bb10527911be90d23c04b69219d120ba6a055
karolinasmile@mail.ru <karolinasmile@mail.ru>
2023-05-01 18:10:54 +0300

    add:catalogItemsMap

--- src/components/Catalog/Battery/Battery.tsx
+++ src/components/Catalog/Battery/Battery.tsx
@@ -29,6 +29,9 @@ export const Battery = () => {
                     <CatalogItems img={walkie} nameItem={'Основные характеристики'} price={'34,00'} descriptionItem={'gdhdhhdhdh'}/>
                     <CatalogItems img={walkie} nameItem={'Основные характеристики'} price={'34,00'} descriptionItem={'gdhdhhdhdh'}/>
                     <CatalogItems img={walkie} nameItem={'Основные характеристики'} price={'34,00'} descriptionItem={'gdhdhhdhdh'}/>
+                    <CatalogItems img={walkie} nameItem={'Основные характеристики'} price={'34,00'} descriptionItem={'gdhdhhdhdh'}/>
+                    <CatalogItems img={walkie} nameItem={'Основные характеристики'} price={'34,00'} descriptionItem={'gdhdhhdhdh'}/>
+                    <CatalogItems img={walkie} nameItem={'Основные характеристики'} price={'34,00'} descriptionItem={'gdhdhhdhdh'}/>
 
 
                 </div></div>
